cc60aea71a5364dabd8e73cf428086d26003e223
Brian Simoni <brsimoni@gmail.com>
2025-02-15 14:45:54 -0500

    fresh

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-function handler(_req: Request): Response {
-  return new Response("what is this?!");
-}
-Deno.serve(handler);
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+import "$std/dotenv/load.ts";
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+import config from "./fresh.config.ts";
+
+await start(manifest, config);
