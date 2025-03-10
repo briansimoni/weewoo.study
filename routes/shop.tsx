706e2e90e661dfc17dd7d7f819239da2bd272259
Brian Simoni <brsimoni@gmail.com>
2025-03-09 21:50:07 -0400

    Start working on the shop section

--- routes/shop.tsx
+++ routes/shop.tsx
@@ -1,28 +1,47 @@
 import { defineRoute } from "$fresh/server.ts";
 import { ShoppingBag } from "../icons/index.ts";
 
+const items = [
+  {
+    "id": 377478087,
+    "external_id": "67cded91916eb9",
+    "name": "weewoo.study hoodie",
+    "variants": 66,
+    "synced": 66,
+    "thumbnail_url":
+      "https://files.cdn.printful.com/files/c07/c0732453e5253619cc78b2213ea92479_preview.png",
+    "is_ignored": false,
+    title: "Premium Hoodie",
+    description: "A premium hoodie for premium people.",
+    price: 29.99,
+  },
+];
+
 export default defineRoute((req, ctx) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-base-100 text-center p-4">
-      <ShoppingBag className="size-16 text-primary mb-6 animate-pulse" />
-      <h1 className="text-4xl font-bold text-base-content mb-4">
-        Shop Coming Soon!
-      </h1>
-      <p className="text-lg text-base-content/80 max-w-md mb-6">
-        We're crafting something special for you. Check back soon!
-      </p>
-      <div className="w-1/2 mb-6">
-        <progress
-          className="progress progress-primary"
-          value="70"
-          max="100"
-        >
-        </progress>
-        <p className="text-sm text-base-content/60">69% Complete</p>
-      </div>
-      <a href="/" className="btn btn-primary">
-        Back to Home
-      </a>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 mr-10 ml-10 lg:mr-30 lg:ml-30 xl:ml-50 xl:mr-50">
+      {items.map((item, index) => (
+        <div className="card border-info-content shadow-2xl">
+          <figure>
+            <img
+              src="https://files.cdn.printful.com/files/c07/c0732453e5253619cc78b2213ea92479_preview.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{item.title}</h2>
+            <p>
+              {item.description}
+            </p>
+            <div className="card-actions flex justify-around">
+              <div className="card-actions text-2xl">
+                ${item.price}
+              </div>
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 });
